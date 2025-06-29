fcd2ba12bbea203065dd252423f63c0cdd91c465
messkely <momomido712@gmail.com>
2025-06-29 15:20:36 +0100

    done

--- alx_backend_graphql_crm/urls.py
+++ alx_backend_graphql_crm/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
-from schema import schema
+from graphql_crm.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
