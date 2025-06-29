629051b13df2132dde30ee99871dbf261082dfc5
messkely <momomido712@gmail.com>
2025-06-29 12:44:47 +0100

    done

--- crm/schema.py
+++ crm/schema.py
@@ -1,13 +1,9 @@
-import graphene
-from crm.schema import Query as CRMQuery, Mutation as CRMMutation
+from graphene_django.filter import DjangoFilterConnectionField
+from .models import Customer, Product, Order
+from .filters import CustomerFilter, ProductFilter, OrderFilter
+from .types import CustomerNode, ProductNode, OrderNode  # Assuming you used DjangoObjectType
 
-
-class Query(CRMQuery, graphene.ObjectType):
-    pass
-
-
-class Mutation(CRMMutation, graphene.ObjectType):
-    pass
-
-
-schema = graphene.Schema(query=Query, mutation=Mutation)
+class Query(graphene.ObjectType):
+    all_customers = DjangoFilterConnectionField(CustomerNode, filterset_class=CustomerFilter)
+    all_products = DjangoFilterConnectionField(ProductNode, filterset_class=ProductFilter)
+    all_orders = DjangoFilterConnectionField(OrderNode, filterset_class=OrderFilter)
